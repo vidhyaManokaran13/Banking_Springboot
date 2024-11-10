95ee26c241a525305d7dfc88ec421ff73ddec814
Vidhya Manokaran <githubvidhya13@gmail.com>
2024-11-10 22:04:31 +0530

    Delete src/main/java/com/example/springboot/Repositary/CourseRepositary.java

--- src/main/java/com/example/springboot/Repositary/CourseRepositary.java
+++
@@ -1,7 +0,0 @@
-package com.example.springboot.Repositary;
-
-import com.example.springboot.Model.CourseModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepositary extends JpaRepository<CourseModel,Integer> {
-}
